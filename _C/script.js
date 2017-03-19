2eec133ca9c073cf7e60cc58d7a1fe351022ff80
denass77 <denass77@mail.ru>
2017-03-19 03:25:37 -0400

    first commit

---
+++ _C/script.js
@@ -0,0 +1,12 @@
+var button = document.getElementById("cn");
+var celsius = document.getElementById("celsius");
+var farenheit = document.getElementById("farenheit");
+
+function convert() {
+
+  var temperature = Number(celsius.value);
+  farenheit.value = 9 / 5 * temperature + 32;
+  
+}
+
+button.addEventListener("click",convert);
